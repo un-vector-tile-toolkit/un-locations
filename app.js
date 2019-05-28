e151d7304db7335360748d39d93e452978d17d0b
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-05-27 21:41:48 -0400

    update

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+const goto = (map, feature) => {
+  map.flyTo({
+    center: feature.geometry.coordinates,
+    zoom: 18,
+    speed: 0.2,
+    curve: 1
+  })
+}
+
 const main = (geojson) => {
-  console.log(geojson)
+  let i = 0
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'https://hfu.github.io/macrostyle/style.json',
     attributionControl: true,
     hash: true
   })
+  map.on('moveend', e => {
+    console.log(e)
+    goto(map, geojson.features[i++ % geojson.features.length])
+  })
+  goto(map, geojson.features[i])
 }
 
 fetch('https://hfu.github.io/un-locations/un-locations.geojson')
--- bundle.js
+++ bundle.js
@@ -1,14 +1,30 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
+var _goto = function _goto(map, feature) {
+  map.flyTo({
+    center: feature.geometry.coordinates,
+    zoom: 18,
+    speed: 0.2,
+    curve: 1
+  });
+};
+
 var main = function main(geojson) {
-  console.log(geojson);
+  var i = 0;
   var map = new mapboxgl.Map({
     container: 'map',
     style: 'https://hfu.github.io/macrostyle/style.json',
     attributionControl: true,
     hash: true
   });
+  map.on('moveend', function (e) {
+    console.log(e);
+
+    _goto(map, geojson.features[i++ % geojson.features.length]);
+  });
+
+  _goto(map, geojson.features[i]);
 };
 
 fetch('https://hfu.github.io/un-locations/un-locations.geojson').then(function (response) {
